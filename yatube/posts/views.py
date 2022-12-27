05fe025434ec95f2e8d81d881630275b5d25bef8
Олег Гущин <atsterq@gmail.com>
2022-12-27 19:49:09 +0500

    шаблоны

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,9 +3,17 @@ from django.shortcuts import render
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
 
 
 def group_list(request, slug):
     template = 'posts/group_list.html'
-    return render(request, template)
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
