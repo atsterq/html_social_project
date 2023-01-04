57521712c454306c49e22e0d743fdd3203950b1a
atsterq <atsterq@gmail.comd:/Dev/character_creation_module/venv/Scripts/python.exe>
2023-01-04 16:31:25 +0500

    posts update

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,14 +4,11 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class Event(models.Model):
-    name = models.CharField(max_length=200)
-    start_at = models.DateTimeField(auto_now_add=True)
-    description = models.TextField()
-    contact = models.EmailField()
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='events'
+        related_name='posts'
     )
-    location = models.CharField(max_length=400)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def index(request):
-    template = 'posts/index.html'
-    text = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'text': text
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_list(request, slug):
